4396371dc655774a93bebd5cbbbdb7db6ba36918
Abhishek Maliyal <abhishekmaliyal123@gmail.com>
2023-07-10 20:43:18 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,15 @@
+import linkIcon from './link.svg'
+import loader from './loader.svg'
+import copy from './copy.svg'
+import logo from './logo.png'
+import tick from './tick.svg'
+import send from './send.svg'
+
+export {
+    linkIcon,
+    loader,
+    copy,
+    logo,
+    tick,
+    send
+}
